247c7f327ec18ae730e105d2e9fe4fe53088ffb0
sziroks <smoczynskioskar@gmail.com>
2024-04-10 23:41:33 +0200

    started blog site

---
+++ django_learning/my_site/blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def starting_page(request):
+    return render(request, "blog/index.html")
+
+
+def posts(request):
+    return render(request, "blog/all-posts.html")
+
+
+def post_detail(request):
+    pass
