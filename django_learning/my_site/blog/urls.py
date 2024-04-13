3162fddf3eb53e33f34d96fadf818f3aeea5ac6d
sziroks <smoczynskioskar@gmail.com>
2024-04-13 23:32:55 +0200

    Added basic blog functionality

--- django_learning/my_site/blog/urls.py
+++ django_learning/my_site/blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.starting_page, name="starting-page"),
     path("posts/", views.posts, name="posts-page"),
     path("posts/<slug:slug>", views.post_detail, name="post-detail-page"),
+    
 ]
